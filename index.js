21db595a03c452355b7809cc18d913cc6d7f4887
evgenjnr <evgeniyr7@gmail.com>
2024-04-09 11:47:02 +0300

    setup initial drag and drop mative api

--- index.js
+++ index.js
@@ -1,42 +1,106 @@
-window.addEventListener('DOMContentLoaded', function (event) {
-	console.log('DOMContentLoaded');
-	const sourceContainer = document.getElementById('sourceContainer');
-	const targetContainer = document.getElementById('targetContainer');
+window.addEventListener("DOMContentLoaded", function (event) {
+  console.log("DOMContentLoaded");
+  const sourceContainer = document.getElementById("sourceContainer");
+  const targetContainer = document.getElementById("targetContainer");
 
-	const itemCollection = sourceContainer.children;
+  const itemCollection = sourceContainer.children;
 
-	for (const key in itemCollection) {
-		if (Object.hasOwnProperty.call(itemCollection, key)) {
-			const element = itemCollection[key];
+  let currentDraggableElement = null;
 
-			element.id = `${element.className}-${key}`;
-			element.addEventListener('dragstart', function (ev) {
-				// ev.preventDefault();
-				console.log('Drag Start', { ev, trg: ev.target, itemCollection });
-				ev.dataTransfer.setData("application/my-app", ev.target.id);
-				ev.dataTransfer.effectAllowed = "move";
+  for (const key in itemCollection) {
+    if (Object.hasOwnProperty.call(itemCollection, key)) {
+      const element = itemCollection[key];
 
-			})
-		}
-	}
-	// sourceContainer
+      element.id = `${element.className}-${key}`;
+    }
+  }
+  sourceContainer.addEventListener("dragstart", function (ev) {
+    currentDraggableElement = null;
+    // ev.dataTransfer.clearData();
+    // ev.dataTransfer.setData("text/plain", ev.target.id);
+    ev.dataTransfer.effectAllowed = "move";
+    currentDraggableElement = ev.target;
+    const timeId = setTimeout(() => {
+      ev.target.classList.add("placeholder");
+      clearTimeout(timeId);
+    }, 0);
+  });
 
-	targetContainer.addEventListener('dragover', function (ev) {
-		ev.preventDefault();
-		ev.preventDefault();
-		ev.dataTransfer.dropEffect = "move";
-		console.log('DragOver==>', { ev });
+  sourceContainer.addEventListener("dragover", function (ev) {
+    ev.preventDefault();
+    ev.dataTransfer.dropEffect = "move";
+    const currentItemList = Array.from(sourceContainer.children);
 
-	})
+    const currentOverlappedElement = ev.target;
 
-	targetContainer.addEventListener('drop', function (ev) {
-		ev.preventDefault();
-		// ev.dataTransfer.clearData();
-		// ev.dataTransfer.setData('application/x-moz-node', ev.target)
-		const data = ev.dataTransfer.getData("application/my-app");
-		ev.target.appendChild(document.getElementById(data));
-		console.log('Drop', { ev, data });
-		// ev.target.appendChild(data);
+    if (currentOverlappedElement.parentElement === sourceContainer) {
+      console.log(
+        "%c DRAG OVER==> ",
+        "color:red;font-weight:bold;background:white",
+        {
+          currentOverlappedElement,
+          currentDraggableElement,
+        }
+      );
+      /**
+       * itemList
+       * replace elements by index
+       * {
+       * 0: currentTarget,
+       * 1: sibling1,
+       * 2: sibling2,
+       * 3: sibling3,
+       * 4: sibling4,
+       * }
+       * replace current element index with currentOverlapedElementIndex
+       *
+       */
+      const draggableIndexForOverlapped = currentItemList.findIndex(
+        (item) => item.id === currentDraggableElement.id
+      );
+      const overlappedIndexForDraggable = currentItemList.findIndex(
+        (item) => item.id === currentOverlappedElement.id
+      );
+      currentItemList[overlappedIndexForDraggable] = currentDraggableElement;
+      currentItemList[draggableIndexForOverlapped] = currentOverlappedElement;
 
-	})
-})
+      sourceContainer.replaceChildren(...currentItemList);
+    }
+  });
+
+  sourceContainer.addEventListener("dragend", function (ev) {
+    ev.preventDefault();
+    currentDraggableElement?.classList.remove("placeholder");
+  });
+
+  sourceContainer.addEventListener("drop", function (ev) {
+    ev.preventDefault();
+    // console.log(
+    //   "%c Drop COntainer==> ",
+    //   "color:red;font-weight:bold;background:white",
+    //   {
+    //     t: ev,
+    //   }
+    // );
+  });
+
+  //   targetContainer.addEventListener("dragover", function (ev) {
+  //     ev.preventDefault();
+  //     ev.dataTransfer.dropEffect = "move";
+  //     console.log("DragOver==>", { ev });
+  //   });
+
+  //   targetContainer.addEventListener("drop", function (ev) {
+  //     ev.preventDefault();
+  //     // ev.dataTransfer.clearData();
+  //     // ev.dataTransfer.setData('application/x-moz-node', ev.target)
+  //     const data = ev.dataTransfer.getData("application/my-app");
+  //     ev.target.appendChild(document.getElementById(data));
+  //     console.log("Drop", { ev, data });
+  //     // ev.target.appendChild(data);
+  //   });
+});
+//   sourceContainer.insertBefore(
+//     currentDraggableElement,
+//     currentOverlapElement
+//   );

04fd1c0ce986a6dacbf611be3043fd1db6116d9c
evgenjnr <evgeniyr7@gmail.com>
2024-04-10 12:57:23 +0300

    replace and update function as callback

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ window.addEventListener("DOMContentLoaded", function (event) {
 
   const itemCollection = sourceContainer.children;
 
-  let currentDraggableElement = null;
+
 
   for (const key in itemCollection) {
     if (Object.hasOwnProperty.call(itemCollection, key)) {
@@ -14,104 +14,111 @@ window.addEventListener("DOMContentLoaded", function (event) {
       element.id = `${element.className}-${key}`;
     }
   }
-  sourceContainer.addEventListener("dragstart", function (ev) {
-    currentDraggableElement = null;
-    ev.dataTransfer.effectAllowed = "move";
-    currentDraggableElement = ev.target;
-    /**====================== SETUP PLACEHOLDER ======================================= */
-    const timeId = setTimeout(() => {
-      ev.target.classList.add("placeholder");
-      clearTimeout(timeId);
-    }, 0);
-  });
 
-  sourceContainer.addEventListener("dragover", function (ev) {
-    ev.preventDefault();
-    ev.dataTransfer.dropEffect = "move";
+  sourceContainer.addEventListener("dragstart", dragStartHandler);
+  sourceContainer.addEventListener("dragover", dragOverSort);
+  sourceContainer.addEventListener("dragend", dragEndHandler);
+  sourceContainer.addEventListener("drop", dropHandler);
 
-    let currentItemList = Array.from(sourceContainer.children);
-
-    const currentOverlappedElement = ev.target;
-
-    if (currentOverlappedElement.parentElement === sourceContainer) {
-      const currentDraggableElementIndex = currentItemList.findIndex(
-        (item) => item.id === currentDraggableElement.id
-      );
-
-      const overlappedElementIndex = currentItemList.findIndex(
-        (item) => item.id === currentOverlappedElement.id
-      );
-      /**============================ CONFIGURING DRAG AND DROP SORTING ================ */
-      /**======================  LEFT | RIGHT && TOP | BOTTOM SIBLINGS ================= */
-      if (
-        currentDraggableElementIndex - 1 === overlappedElementIndex ||
-        currentDraggableElementIndex + 1 === overlappedElementIndex
-      ) {
-        currentItemList[overlappedElementIndex] = currentDraggableElement;
-        currentItemList[currentDraggableElementIndex] =
-          currentOverlappedElement;
-        /**============================== MULTILINE DRAGGING =========================== */
-        /**============================== FROM LEFT TO RIGHT || TOP TO BOTTOM ========== */
-      } else if (currentDraggableElementIndex + 1 < overlappedElementIndex) {
-        /**============================================================================= */
-        for (
-          let index = currentDraggableElementIndex;
-          index <= overlappedElementIndex;
-          index++
-        ) {
-          if (index !== overlappedElementIndex) {
-            currentItemList[index] = currentItemList[index + 1];
-          } else {
-            currentItemList[index] = currentDraggableElement;
-          }
-        }
-        /**============================== FROM RIGHT TO LEFT || BOTTOM TO TOP ========== */
-      } else {
-        /**============================================================================== */
-        for (
-          let index = currentDraggableElementIndex;
-          index >= overlappedElementIndex;
-          index--
-        ) {
-          if (index !== overlappedElementIndex) {
-            currentItemList[index] = currentItemList[index - 1];
-          } else {
-            currentItemList[index] = currentDraggableElement;
-          }
-        }
-        /**=============================================================================== */
-      }
-
-      sourceContainer.replaceChildren(...currentItemList);
-    }
-  });
-
-  sourceContainer.addEventListener("dragend", function (ev) {
-    ev.preventDefault();
-    currentDraggableElement?.classList.remove("placeholder");
-  });
-
-  sourceContainer.addEventListener("drop", function (ev) {
-    ev.preventDefault();
-  });
-
-  //   targetContainer.addEventListener("dragover", function (ev) {
-  //     ev.preventDefault();
-  //     ev.dataTransfer.dropEffect = "move";
-  //     console.log("DragOver==>", { ev });
-  //   });
-
-  //   targetContainer.addEventListener("drop", function (ev) {
-  //     ev.preventDefault();
-  //     // ev.dataTransfer.clearData();
-  //     // ev.dataTransfer.setData('application/x-moz-node', ev.target)
-  //     const data = ev.dataTransfer.getData("application/my-app");
-  //     ev.target.appendChild(document.getElementById(data));
-  //     console.log("Drop", { ev, data });
-  //     // ev.target.appendChild(data);
-  //   });
+  targetContainer.addEventListener("dragstart", dragStartHandler);
+  targetContainer.addEventListener("dragover", dragOverSort);
+  targetContainer.addEventListener("dragend", dragEndHandler);
+  targetContainer.addEventListener("drop", dropHandler);
 });
-//   sourceContainer.insertBefore(
-//     currentDraggableElement,
-//     currentOverlapElement
-//   );
+
+function dragStartHandler(ev) {
+
+  ev.dataTransfer.effectAllowed = "move";
+  ev.dataTransfer.clearData();
+  ev.dataTransfer.setData('text', ev.target.id)
+
+  /**====================== SETUP PLACEHOLDER ======================================= */
+  const timeId = setTimeout(() => {
+    ev.target.classList.add("dragging");
+    clearTimeout(timeId);
+  }, 0);
+}
+
+function dropHandler(ev) {
+  ev.preventDefault();
+
+
+  if (ev.target === this) {
+    const data = ev.dataTransfer.getData("text");
+    const draggedElement = document.getElementById(data);
+
+    if (!Array.from(this.children).some(item => item === draggedElement)) {
+      draggedElement.classList.remove('dragging');
+      this.appendChild(draggedElement);
+      ev.dataTransfer.clearData();
+    }
+  }
+}
+
+function dragOverSort(ev) {
+  ev.preventDefault();
+  ev.dataTransfer.dropEffect = "move";
+
+  let currentItemList = Array.from(this.children);
+  console.log('DRAGOVER THIS', this);
+  const currentOverlappedElement = ev.target;
+
+  if (currentOverlappedElement.parentElement === this) {
+    const currentDraggableElement = this.querySelector('.dragging');
+    const currentDraggableElementIndex = currentItemList.findIndex(
+      (item) => item.id === currentDraggableElement.id
+    );
+
+    const overlappedElementIndex = currentItemList.findIndex(
+      (item) => item.id === currentOverlappedElement.id
+    );
+    /**============================ CONFIGURING DRAG AND DROP SORTING ================ */
+    /**======================  LEFT | RIGHT && TOP | BOTTOM SIBLINGS ================= */
+    if (
+      currentDraggableElementIndex - 1 === overlappedElementIndex ||
+      currentDraggableElementIndex + 1 === overlappedElementIndex
+    ) {
+      currentItemList[overlappedElementIndex] = currentDraggableElement;
+      currentItemList[currentDraggableElementIndex] =
+        currentOverlappedElement;
+      /**============================== MULTILINE DRAGGING =========================== */
+      /**============================== FROM LEFT TO RIGHT || TOP TO BOTTOM ========== */
+    } else if (currentDraggableElementIndex + 1 < overlappedElementIndex) {
+      /**============================================================================= */
+      for (
+        let index = currentDraggableElementIndex;
+        index <= overlappedElementIndex;
+        index++
+      ) {
+        if (index !== overlappedElementIndex) {
+          currentItemList[index] = currentItemList[index + 1];
+        } else {
+          currentItemList[index] = currentDraggableElement;
+        }
+      }
+      /**============================== FROM RIGHT TO LEFT || BOTTOM TO TOP ========== */
+    } else {
+      /**============================================================================== */
+      for (
+        let index = currentDraggableElementIndex;
+        index >= overlappedElementIndex;
+        index--
+      ) {
+        if (index !== overlappedElementIndex) {
+          currentItemList[index] = currentItemList[index - 1];
+        } else {
+          currentItemList[index] = currentDraggableElement;
+        }
+      }
+      /**=============================================================================== */
+    }
+
+    this.replaceChildren(...currentItemList);
+  }
+}
+
+function dragEndHandler(ev) {
+  ev.preventDefault();
+  this.querySelector('.dragging')?.classList.remove("dragging");
+  console.log('DRAG END DATA', { d: ev.dataTransfer.getData('text') })
+}
